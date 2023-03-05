229e129f76f281edab2b9d502010a92dedee20ce
Ed Cragg <ed.cragg@eipi.xyz>
2023-03-05 18:42:09 +0000

    All in the header

--- include/superFreq.h
+++ include/superFreq.h
@@ -55,4 +55,144 @@ private:
   uint32_t lastHigh = 0;
 };
 
+template <size_t N, typename I>
+void superFreq<N,I>::update(bool state) {
+  uint32_t m = micros();
+  uint32_t p = m - lastHigh;
+  lastHigh = m;
+
+  switch (state) {
+    case true:
+      periods.insert(p);
+      break;
+    case false:
+      highPeriods.insert(p);
+      break;
+  }
+}
+
+template <size_t N, typename I>
+float superFreq<N,I>::getFreq() {
+  return 1000000.0 / (float)getPeriod();
+
+}
+
+template <size_t N, typename I>
+float superFreq<N,I>::getPulseWidth() {
+  return (float)getHighPeriod() / (float)getLowPeriod();
+
+}
+
+template <size_t N, typename I>
+uint32_t superFreq<N,I>::getPeriod() {
+  return periods.getAvg();
+}
+
+template <size_t N, typename I>
+uint32_t superFreq<N,I>::getHighPeriod() {
+  return highPeriods.getAvg();
+}
+
+template <size_t N, typename I>
+uint32_t superFreq<N,I>::getLowPeriod() {
+  return periods.getAvg() - highPeriods.getAvg();
+}
+
+template <size_t N, typename I>
+I superFreq<N,I>::available() {
+
+}
+
+template <size_t N, typename I>
+superFreqEdge superFreq<N,I>::readEdge() {
+
+}
+
+#ifdef SUPER_FREQ_DEBUG
+template <size_t N, typename I>
+void superFreq<N,I>::print() {
+  periods.print();
+  highPeriods.print();
+}
+#endif
+
+template <size_t N, typename T, typename I>
+void superFreqRingBuffer<N,T,I>::insert(T value) {
+  buffer[h] = value;
+  advance();
+}
+
+template <size_t N, typename T, typename I>
+T superFreqRingBuffer<N,T,I>::getAvg() {
+  I checksum = h ^ t;
+  if(checksum != chk) {
+    avg = calcAvg();
+    chk = checksum;
+  }
+  return avg;
+}
+
+/* calculate an average of last n period values
+ *
+ * start from oldest sample to avoid data changing in interrupt as much as
+ * possible
+ */
+template <size_t N, typename T, typename I>
+T superFreqRingBuffer<N,T,I>::calcAvg() {
+  uint32_t sum = 0;
+  I count = 0;
+  I start = t;
+
+  while ((start++ % N) != h) {
+    sum += buffer[(start + count++) % N];
+  }
+
+  return ((sum << 3) / count) >> 3;
+}
+
+template <size_t N, typename T, typename I>
+T superFreqRingBuffer<N,T,I>::read() {
+
+}
+
+template <size_t N, typename T, typename I>
+I superFreqRingBuffer<N,T,I>::available() {
+  return (I)(((int32_t)h - (int32_t)t) % (int32_t)N);
+}
+
+template <size_t N, typename T, typename I>
+bool superFreqRingBuffer<N,T,I>::isFull() {
+  return ((h + 1) % N == t);
+}
+
+template <size_t N, typename T, typename I>
+bool superFreqRingBuffer<N,T,I>::isEmpty() {
+  return (h == t);
+}
+
+template <size_t N, typename T, typename I>
+void superFreqRingBuffer<N,T,I>::advance() {
+  if(isFull()) {
+		t = (t + 1) % N; /* advance tail if full */
+    if (t > r) {
+		  r = (r + 1) % N; /* advance read if tail has caught up */
+    }
+	}
+	h = (h + 1) % N; /* advance head */
+}
+
+#ifdef SUPER_FREQ_DEBUG
+template <size_t N, typename T, typename I>
+void superFreqRingBuffer<N,T,I>::print() {
+  for (I i=0; i<N; i++) {
+    if (i == t) Serial.print("T(");
+    if (i == h) Serial.print("H(");
+    if (i == r) Serial.print("R(");
+    Serial.print(buffer[i]);
+    Serial.print(' ');
+    if (i == t || i == h || i == r) Serial.print(")");
+  }
+}
+#endif
+
 #endif // __SUPER_FREQ__
