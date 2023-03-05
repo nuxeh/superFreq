bfcdee31cb6df1d88fa42567ca14fa73d2e6e4ac
Ed Cragg <ed.cragg@eipi.xyz>
2023-03-05 10:26:02 +0000

    ...

--- src/superFreqRingBuffer.cpp
+++ src/superFreqRingBuffer.cpp
@@ -2,17 +2,7 @@
 
 template <size_t N, typename T, typename I>
 void superFreqRingBuffer<N,T,I>::insert(T value) {
-  
-}
-
-template <size_t N, typename T, typename I>
-bool superFreqRingBuffer<N,T,I>::isFull() {
-  return (h + 1 == t);
-}
-
-template <size_t N, typename T, typename I>
-bool superFreqRingBuffer<N,T,I>::isEmpty() {
-  return (h == t);
+  buffer[h] = value;
 }
 
 template <size_t N, typename T, typename I>
@@ -38,4 +28,25 @@ T superFreqRingBuffer<N,T,I>::read() {
 template <size_t N, typename T, typename I>
 I superFreqRingBuffer<N,T,I>::available() {
 
+}
+
+template <size_t N, typename T, typename I>
+bool superFreqRingBuffer<N,T,I>::isFull() {
+  return (h + 1 == t);
+}
+
+template <size_t N, typename T, typename I>
+bool superFreqRingBuffer<N,T,I>::isEmpty() {
+  return (h == t);
+}
+
+template <size_t N, typename T, typename I>
+void superFreqRingBuffer<N,T,I>::advance() {
+  if(isFull()) {
+		t = (t + 1) % N; /* advance tail if full */
+    if (t == r) {
+		  r = (r + 1) % N; /* advance read if tail has caught up */
+    }
+	}
+	h = (h + 1) % N; /* advance head */
 }
