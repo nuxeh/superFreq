aa4c4354c634b08f3c410a96cc90e6861b7a73d0
Ed Cragg <ed.cragg@eipi.xyz>
2023-03-06 00:24:53 +0000

    Fix read pointer rotation
    150000 H(150000) T(R(150000) 150000 150000
    6.67
    150000 H(150000) T(R(150000) 150000 150000
    6.67
    150000 150000 H(150000) T(R(150000) 150000
    0 3 3
    1 4 4
    2 5 0
    3 6 1
    Recalculating...
    6.67
    150000 150000 H(150000) T(R(150000) 150000
    6.67
    150000 150000 H(150000) T(R(150000) 150000
    6.67
    150000 150000 150000 H(150000) T(R(150000)
    0 4 4
    1 5 0
    2 6 1
    3 7 2
    Recalculating...
    6.67
    150000 150000 150000 H(150000) T(R(150000)
    6.67
    150000 150000 150000 H(150000) T(R(150000)
    6.67
    T(R(150000) 150000 150000 150000 H(150000)
    0 0 0
    1 1 1
    2 2 2
    3 3 3
    Recalculating...
    6.67
    T(R(150000) 150000 150000 150000 H(150000)
    6.67
    T(R(150000) 150000 150000 150000 H(150000)
    6.67
    H(150000) T(R(150000) 150000 150000 150000

--- include/superFreq.h
+++ include/superFreq.h
@@ -181,10 +181,10 @@ bool superFreqRingBuffer<N,T,I>::isEmpty() {
 template <size_t N, typename T, typename I>
 void superFreqRingBuffer<N,T,I>::advance() {
   if(isFull()) {
-		t = (t + 1) % N; /* advance tail if full */
     if (t + 1 > r) {
 		  r = (r + 1) % N; /* advance read if tail has caught up */
     }
+		t = (t + 1) % N; /* advance tail if full */
 	}
 	h = (h + 1) % N; /* advance head */
 }
